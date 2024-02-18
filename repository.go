601cdfe7e9629c98e8c41ca0176cf3f0ab76ffe5
emanuel-xavier <emanueljx@outlook.com>
2024-02-17 22:13:58 -0300

    add function that get the balance of one client

--- repository.go
+++ repository.go
@@ -1,9 +1,31 @@
 package main
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"strconv"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type ClientRepository struct {
 	dbPool *pgxpool.Pool
+}
+
+func (cm *ClientRepository) getClientBalance(c context.Context, id int) (*BalanceDto, error) {
+	var b BalanceDto
+
+	idStr := strconv.Itoa(id)
+	row := cm.dbPool.QueryRow(c,
+		"SELECT \"limit\", balance FROM \"clients\" WHERE id = $1",
+		idStr,
+	)
+
+	err := row.Scan(&b.Limit, &b.Total)
+	if err != nil {
+		return nil, err
+	}
+
+	return &b, nil
 }
 
 type TransactionRepository struct {
