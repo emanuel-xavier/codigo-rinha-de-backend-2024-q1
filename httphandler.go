3c55db9ff465586cfbbe5aea7de88b0915bfcabc
emanuel-xavier <emanueljx@outlook.com>
2024-02-17 22:48:28 -0300

    initilize pool and repositories

--- httphandler.go
+++ httphandler.go
@@ -5,8 +5,12 @@ import (
 	"log"
 
 	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/configs"
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/db"
 	"github.com/gofiber/fiber/v2"
 )
+
+var cr *ClientRepository
+var tr *TransactionRepository
 
 func createTransactionHandler(c *fiber.Ctx) error {
 	return nil
@@ -22,6 +26,12 @@ func Handle() {
 		log.Fatal(fmt.Errorf("Failed to load configs %s", err.Error()))
 	}
 	serverPort := ":" + configs.GetServerPort()
+
+	db.Initialize()
+	pool := db.GetPool()
+
+	cr = NewClientRepository(pool)
+	tr = NewTransactionRepository(pool)
 
 	app := fiber.New()
 
