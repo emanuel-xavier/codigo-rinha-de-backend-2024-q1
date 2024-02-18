861364d39f57375fe266d6318e0e9fa2d7f36457
emanuel-xavier <emanueljx@outlook.com>
2024-02-18 17:14:29 -0300

    add validation to CreateTransacitonDto and change the name of the field 'T' to 'Type'

--- type.go
+++ type.go
@@ -1,11 +1,35 @@
 package main
 
-import "time"
+import (
+	"log"
+	"strings"
+	"time"
+)
 
 type CreateTransactionDto struct {
 	Value       int    `json:"valor"`
-	T           rune   `json:"tipo"`
+	Type        string `json:"tipo"`
 	Description string `json:"descricao"`
+}
+
+func (ctDto *CreateTransactionDto) validate() bool {
+	log.Println(*ctDto)
+	if ctDto.Value <= 0 {
+		log.Println("Value <= 0")
+		return false
+	}
+
+	if strings.ToLower(ctDto.Type) != "c" && strings.ToLower(ctDto.Type) != "d" {
+		log.Println("Wrong type")
+		return false
+	}
+
+	if decLen := len(ctDto.Description); decLen < 1 || decLen > 10 {
+		log.Println("Description size")
+		return false
+	}
+
+	return true
 }
 
 type TransactionResponseDto struct {
