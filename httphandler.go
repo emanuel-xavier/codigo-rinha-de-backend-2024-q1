fd58d44fc95b36da1821ad0a479de9f163f2f5b6
emanuel-xavier <emanueljx@outlook.com>
2024-02-20 15:50:40 -0300

    createTransaction http handler v1 working

--- httphandler.go
+++ httphandler.go
@@ -4,17 +4,51 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/configs"
 	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/db"
 	"github.com/gofiber/fiber/v2"
 )
 
-var cr *ClientRepository
-var tr *TransactionRepository
+var (
+	ts *TransactionService
+	cs *ClientService
+)
 
 func createTransactionHandler(c *fiber.Ctx) error {
-	return nil
+	id := c.Params("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		log.Println("Failed to parse id to string: ", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	var trDto CreateTransactionDto
+	if err := c.BodyParser(&trDto); err != nil {
+		log.Println("Error parsing request body: ", err)
+		return c.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+
+	if !trDto.validate() {
+		log.Println("Cant validate request body content")
+		return c.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+
+	trResp, err := ts.CreateTransaction(c.Context(), idInt, trDto)
+	if err != nil {
+		log.Println(err)
+		switch err {
+		case ErrInsufficientBalance:
+			return c.SendStatus(fiber.StatusUnprocessableEntity)
+		case ErrNotFound:
+			return c.SendStatus(fiber.StatusNotFound)
+		default:
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+	}
+
+	return c.Status(fiber.StatusOK).JSON(trResp)
 }
 
 func getStatementHandler(c *fiber.Ctx) error {
@@ -31,8 +65,8 @@ func Handle() {
 	db.Initialize()
 	pool := db.GetPool()
 
-	cr = NewClientRepository(pool)
-	tr = NewTransactionRepository(pool)
+	cs = NewClientService(pool)
+	ts = NewTransactionService(pool)
 
 	app := fiber.New(fiber.Config{
 		JSONEncoder: json.Marshal,
@@ -42,5 +76,6 @@ func Handle() {
 	app.Post("/clientes/:id/transacoes", createTransactionHandler)
 	app.Get("/clientes/:id/extrato", getStatementHandler)
 
+	log.Println("Server runing on port", serverPort)
 	log.Fatal(app.Listen(serverPort))
 }
