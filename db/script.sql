0d4ebc86dba39adb10233975aac4fef94d7e8357
emanuel-xavier <emanueljx@outlook.com>
2024-02-21 22:27:28 -0300

    add default users to the database

--- db/script.sql
+++ db/script.sql
@@ -13,3 +13,14 @@ CREATE TABLE IF NOT EXISTS "transaction" (
   "created_at"  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   "client_id"   INT NOT NULL REFERENCES clients("id")
 );
+
+DO $$
+BEGIN
+  INSERT INTO clients ("name", "limit")
+  VALUES
+    ('o barato sai caro', 1000 * 100),
+    ('zan corp ltda', 800 * 100),
+    ('les cruders', 10000 * 100),
+    ('padaria joia de cocaia', 100000 * 100),
+    ('kid mais', 5000 * 100);
+END; $$;
