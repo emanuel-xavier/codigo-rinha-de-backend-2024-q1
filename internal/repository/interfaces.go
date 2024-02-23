7726d65534af32fcfd45180a9cf40f12297afa8e
emanuel-xavier <emanueljx@outlook.com>
2024-02-22 22:55:07 -0300

    adds reporitory interfaces

---
+++ internal/repository/interfaces.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/internal/entity"
+)
+
+type ClientRepository interface {
+	GetClientById(id int) (entity.Client, error)
+}
+
+type TransactionRepository interface {
+	GetTenLastTransactionsByUserId(ctx context.Context, id int) ([]entity.Transaction, error)
+	CreateTransaction(ctx context.Context, transaction entity.Transaction, balance int) error
+}
