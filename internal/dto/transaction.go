9110b57b1556ec7ad76133176daf7592e13d409d
emanuel-xavier <emanueljx@outlook.com>
2024-02-23 09:47:45 -0300

    fixes some json tags and fixes error message

--- internal/dto/transaction.go
+++ internal/dto/transaction.go
@@ -3,19 +3,19 @@ package dto
 import "errors"
 
 type TransactionRequest struct {
-	Value       int    `json:"value"`
+	Value       int    `json:"valor"`
 	Type        string `json:"tipo"`
 	Description string `json:"descricao"`
 }
 
 type TransactionResponse struct {
-	Limit   int `json:limite`
+	Limit   int `json:"limite"`
 	Balance int `json:"saldo"`
 }
 
 func (tr *TransactionRequest) Validate() error {
 	if tr.Value <= 0 {
-		return errors.New("the value must be greater than 10")
+		return errors.New("the value must be greater than 0")
 	}
 
 	if len(tr.Description) > 10 || tr.Description == "" {
