4c9e45c6a1e2554c4d661e09a9d0e88bb15788fe
emanuel-xavier <emanueljx@outlook.com>
2024-02-22 22:05:02 -0300

    adds dto for create traction route

---
+++ internal/dto/transaction.go
@@ -0,0 +1,30 @@
+package dto
+
+import "errors"
+
+type TransactionRequest struct {
+	Value       int    `json:"value"`
+	Type        string `json:"tipo"`
+	Description string `json:"descricao"`
+}
+
+type TransactionResponse struct {
+	Limit   int `json:limite`
+	Balance int `json:"saldo"`
+}
+
+func (tr *TransactionRequest) Validate() error {
+	if tr.Value <= 0 {
+		return errors.New("the value must be greater than 10")
+	}
+
+	if len(tr.Description) > 10 || tr.Description == "" {
+		return errors.New("the description must not be empty or have more than 10 characters")
+	}
+
+	if tr.Type != "c" && tr.Type != "d" {
+		return errors.New("the type value must be 'c' or 'd'")
+	}
+
+	return nil
+}
