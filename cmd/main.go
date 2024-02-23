d576e0650624ae14c23ff8aa23e67c04c5f22c0b
emanuel-xavier <emanueljx@outlook.com>
2024-02-23 10:50:10 -0300

    adds API start point

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/configs"
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/db"
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/internal/repository/postgres"
 	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/internal/router"
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/internal/service"
 )
 
 func main() {
-	var r router.FiberRouter
+	err := configs.Load()
+	if err != nil {
+		panic("Failed to load configs")
+	}
+	db.Initialize()
+	pool := db.GetPool()
+
+	cRepo := postgres.NewClientRepo(pool)
+	tRepo := postgres.NewTransactionRepo(pool)
+
+	cService := service.NewClientService(cRepo, tRepo)
+	tService := service.NewTransactionService(cRepo, tRepo)
+
+	r := router.NewFiberRouter(cService, tService)
 	r.Init()
 }
