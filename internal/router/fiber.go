83b52965553ffbd9f1689c6b5817257eb6ff03fb
emanuel-xavier <emanueljx@outlook.com>
2024-02-23 00:30:09 -0300

    adds fiber routes, and http handler to create a transaction

--- internal/router/fiber.go
+++ internal/router/fiber.go
@@ -1,15 +1,28 @@
 package router
 
 import (
+	"context"
 	"encoding/json"
 	"log"
+	"strconv"
 
 	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/configs"
-	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/db"
-	fiber "github.com/gofiber/fiber/v2"
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/internal/dto"
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/internal/entity"
+	"github.com/emanuel-xavier/codigo-rinha-de-backend-2024-q1/internal/service"
+	"github.com/gofiber/fiber/v2"
 )
 
 type FiberRouter struct {
+	cs *service.ClientService
+	ts *service.TransactionService
+}
+
+func NewFiberRouter(cs *service.ClientService, ts *service.TransactionService) *FiberRouter {
+	return &FiberRouter{
+		cs: cs,
+		ts: ts,
+	}
 }
 
 func (fr *FiberRouter) Init() {
@@ -20,8 +33,6 @@ func (fr *FiberRouter) Init() {
 
 	serverPort := ":" + configs.GetServerPort()
 
-	db.Initialize()
-
 	app := fiber.New(fiber.Config{
 		JSONEncoder: json.Marshal,
 		JSONDecoder: json.Unmarshal,
@@ -31,5 +42,50 @@ func (fr *FiberRouter) Init() {
 		return c.SendStatus(fiber.StatusOK)
 	})
 
+	app.Post("/clientes/:id/transacoes", fr.createTransactionHandler)
+	app.Get("/clientes/:id/extrato", fr.getStatementHandler)
+
 	log.Fatal(app.Listen(serverPort))
+}
+
+func (fr *FiberRouter) createTransactionHandler(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	var trDto dto.TransactionRequest
+	if err := ctx.BodyParser(&trDto); err != nil {
+		return ctx.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+
+	if err := trDto.Validate(); err != nil {
+		return ctx.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+
+	client, err := fr.cs.GetClientById(context.Background(), idInt)
+	if err != nil {
+		if err.Error() == "not found" {
+			return ctx.SendStatus(fiber.StatusNotFound)
+		}
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	transaction := entity.Transaction{
+		Type:       trDto.Type,
+		Descrition: trDto.Description,
+		Value:      trDto.Value,
+		ClientId:   idInt,
+	}
+
+	if err := fr.ts.CreateTransaction(ctx.Context(), transaction, client.Balance); err != nil {
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return nil
+}
+
+func (fr *FiberRouter) getStatementHandler(ctx *fiber.Ctx) error {
+	return nil
 }
