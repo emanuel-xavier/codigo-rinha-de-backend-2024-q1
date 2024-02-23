a2a399528a90db4963c7fd7b087a82f22e6839a3
emanuel-xavier <emanueljx@outlook.com>
2024-02-22 23:11:34 -0300

    adds client id and fixes json format for Value

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -6,6 +6,7 @@ type Transaction struct {
 	Id           int       `json:"id,omitempty"`
 	Type         string    `json:"tipo"`
 	Descrition   string    `json:"descricao"`
-	Value        int       `json:"valor,omitempty`
-	Accomplished time.Time `json:"realizado_em"`
+	Value        int       `json:"valor,omitempty"`
+	Accomplished time.Time `json:"realizado_em,omitempty"`
+	ClientId     int       `json:"id_do_cliente,omitempty"`
 }
